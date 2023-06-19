796d260d266f144aa0f991276dde644053cf2fab
Kumar Utsav <21051903@kiit.ac.in>
2023-06-19 14:43:37 +0530

    Lecture 004 Done, activity_main.xml done.

--- app/src/main/java/com/krutsav/s23quizapp/MainActivity.kt
+++ app/src/main/java/com/krutsav/s23quizapp/MainActivity.kt
@@ -2,10 +2,16 @@ package com.krutsav.s23quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // TODO: below method is depreciated, learn the alternative way to make activity full screen
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+
     }
 }
